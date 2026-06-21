85b530da055c41b87fee6f7cb203df2ddb98c320
Mina Kamel <minallkamel@gmail.com>
2026-06-21 16:46:05 +0300

    refactor

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from PySide6.QtCore import Qt, QDir
 import sys
 from merge_widget import MergeWidget
 from split_widget import SplitWidget
+from delete_widget import DeleteWidget
+from reorder_widget import ReorderWidget
 
 
 class MainWindow(QWidget):
@@ -34,7 +36,7 @@ class MainWindow(QWidget):
         self.splitter.setStretchFactor(1, 1)
         self.stack = QStackedWidget()
 
-        self.actions = ["Merge", "Split", "Delete", "Rotate"]
+        self.actions = ["Merge", "Split", "Reorder", "Delete"]
         self.left_box_list = QListWidget()
         self.left_box_list.addItems(self.actions)
         self.left_box_list.currentRowChanged.connect(self.stack.setCurrentIndex)
@@ -44,8 +46,13 @@ class MainWindow(QWidget):
 
         self.merge_widget = MergeWidget()
         self.split_widget = SplitWidget()
+        self.reorder_widget = ReorderWidget()
+        self.delete_widget = DeleteWidget()
+
         self.stack.addWidget(self.merge_widget)
         self.stack.addWidget(self.split_widget)
+        self.stack.addWidget(self.reorder_widget)
+        self.stack.addWidget(self.delete_widget)
 
         self.layout.addWidget(self.splitter)
 
---
+++ reorder_widget.py
@@ -0,0 +1,57 @@
+from PySide6.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QListWidget,
+    QListWidgetItem,
+    QApplication,
+    QSplitter,
+    QPushButton,
+    QStackedWidget,
+    QFileDialog,
+    QHBoxLayout,
+    QLineEdit,
+    QLabel,
+)
+from PySide6.QtCore import Qt, QDir
+
+
+class ReorderWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.layout = QVBoxLayout(self)
+
+        self.pages_widget = QWidget()
+        self.pages_layout = QVBoxLayout(self.pages_widget)
+        self.file_label = QLabel("File:")
+        self.pages_list = QListWidget()
+        self.pages_layout.addWidget(self.file_label, 0, Qt.AlignmentFlag.AlignCenter)
+        self.pages_layout.addWidget(self.pages_list)
+
+        self.controls_widget = QWidget()
+        self.controls_layout = QHBoxLayout(self.controls_widget)
+        self.add_file_btn = QPushButton("Add File")
+        self.add_file_btn.clicked.connect(self.open_file)
+
+        self.split_btn = QPushButton("Reorder")
+        self.split_btn.setEnabled(False)
+
+        self.pages_list.model().rowsInserted.connect(self.on_pages_change)
+        self.pages_list.model().rowsRemoved.connect(self.on_pages_change)
+
+        self.controls_layout.addWidget(self.add_file_btn)
+        self.controls_layout.addStretch()
+        self.controls_layout.addWidget(self.split_btn)
+
+        self.layout.addWidget(self.pages_widget)
+        self.layout.addStretch()
+        self.layout.addWidget(self.controls_widget)
+
+    def open_file(self):
+        self.path, _ = QFileDialog.getOpenFileName(
+            self, "Open File", QDir.homePath(), "PDFs (*.pdf)"
+        )
+        self.file_label.setText(f"File: {self.path}")
+
+    def on_pages_change(self):
+        self.pages_input.setEnabled(self.pages_list.count() > 0)
+        self.split_btn.setEnabled(self.pages_list.count() > 0)
