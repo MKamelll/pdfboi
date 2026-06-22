7e7d36a52603d405978a60cfe270b50e2fc80aff
Mina Kamel <minallkamel@gmail.com>
2026-06-22 08:45:30 +0300

    filter the displayed pages

--- split_widget.py
+++ split_widget.py
@@ -43,8 +43,11 @@ class SplitWidget(QWidget):
 
         self.pages_input = QLineEdit()
         self.pages_input.setPlaceholderText("pages (i.e 1,3-5,4)")
-        self.split_btn = QPushButton("Split")
         self.pages_input.setEnabled(False)
+        self.pages_input.textChanged.connect(self.calculate_indices)
+        self.pages_input.textChanged.connect(self.re_render_thumbnails)
+
+        self.split_btn = QPushButton("Split")
         self.split_btn.setEnabled(False)
 
         self.pages_list.model().rowsInserted.connect(self.on_pages_change)
@@ -67,10 +70,15 @@ class SplitWidget(QWidget):
     def render_thumbnails(self):
         self.viewer_worker = ThumbnailWorker(self.path)
         self.viewer_worker.total_ready.connect(self.progress_bar.setMaximum)
+        self.viewer_worker.total_ready.connect(self.set_initial_indices)
         self.viewer_worker.total_ready.connect(self.prepopulate_list)
         self.viewer_worker.progress.connect(self.progress_bar.setValue)
         self.viewer_worker.results_ready.connect(self.on_page_ready)
         self.viewer_worker.start()
+
+    def set_initial_indices(self, count: int):
+        self.page_count = count
+        self.pages_indices = [i for i in range(count)]
 
     def prepopulate_list(self, count: int):
         for i in range(count):
@@ -91,3 +99,33 @@ class SplitWidget(QWidget):
     def on_pages_change(self):
         self.pages_input.setEnabled(self.pages_list.count() > 0)
         self.split_btn.setEnabled(self.pages_list.count() > 0)
+
+    def calculate_indices(self, text: str):
+        gps = text.split(",")
+        results = set()
+        try:
+            for g in gps:
+                if "-" in g:
+                    parts = g.split("-")
+                    if len(parts) == 2:
+                        start, end = int(parts[0]) - 1, int(parts[1]) - 1
+                        start = max(0, min(start, self.page_count))
+                        end = max(0, min(end, self.page_count))
+                        for i in range(start, end + 1):
+                            results.add(i)
+                else:
+                    g_int = max(0, min(int(g) - 1, self.page_count))
+                    results.add(g_int)
+        except ValueError:
+            pass
+
+        self.pages_indices = list(results)
+
+    def re_render_thumbnails(self, text: str):
+        if len(text) == 0:
+            for i in range(self.pages_list.count()):
+                self.pages_list.setRowHidden(i, False)
+
+        else:
+            for i in range(self.pages_list.count()):
+                self.pages_list.setRowHidden(i, i not in self.pages_indices)
