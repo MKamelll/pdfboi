15ba655a9d5a58bbe46ac4c30201f1278c8a8a62
Mina Kamel <minallkamel@gmail.com>
2026-06-24 10:52:34 +0300

    just one file at a time

--- convertto_widget.py
+++ convertto_widget.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import (
     QCheckBox,
     QComboBox,
+    QLabel,
     QWidget,
     QVBoxLayout,
     QListWidget,
@@ -17,6 +18,8 @@ from PySide6.QtWidgets import (
 from PySide6.QtCore import Qt, QDir, QThread, Signal
 from pypdf import PdfWriter
 
+from pdflist_widget import PdfListWidget
+
 
 class Worker(QThread):
     results_ready = Signal(PdfWriter)
@@ -24,52 +27,44 @@ class Worker(QThread):
     total_ready = Signal(int)
     error = Signal(str)
 
-    def __init__(self, files: list[str]):
+    def __init__(self, path: str):
         super().__init__()
-        self.files = files
+        self.path = path
 
     def run(self) -> None:
-        if len(self.files) < 1:
-            self.error.emit("Files cannot be empty")
-
-        self.total_ready.emit(len(self.files))
-
-        merger = PdfWriter()
-
-        for i, path in enumerate(self.files):
-            merger.append(path)
-            self.progress.emit(i + 1)
-
-        self.results_ready.emit(merger)
+        pass
 
 
 class ConvertToWidget(QWidget):
     def __init__(self) -> None:
         super().__init__()
         self._layout = QVBoxLayout(self)
-        self.file_list = QListWidget()
+        self.path: str | None = None
         self.progress_bar = QProgressBar()
         self.progress_bar.hide()
+
+        self.file_label = QLabel("File:")
+        self.list_widget = PdfListWidget()
+        self.list_widget.started.connect(self.progress_bar.show)
+        self.list_widget.progress_max.connect(self.progress_bar.setMaximum)
+        self.list_widget.progress_update.connect(self.progress_bar.setValue)
+        self.list_widget.done.connect(self.progress_bar.hide)
+        self.list_widget.done.connect(self.progress_bar.reset)
+        self.list_widget.pages_change.connect(self.on_pages_change)
+        self._layout.addWidget(self.file_label, 0, Qt.AlignmentFlag.AlignCenter)
+        self._layout.addWidget(self.list_widget)
 
         self.controls_widget = QWidget()
         self.controls_layout = QHBoxLayout(self.controls_widget)
 
-        self.add_files_btn = QPushButton("Add Files")
-        self.add_files_btn.clicked.connect(self.open_files)
-        self.controls_layout.addWidget(self.add_files_btn)
+        self.add_file_btn = QPushButton("Add File")
+        self.add_file_btn.clicked.connect(self.open_file)
+        self.controls_layout.addWidget(self.add_file_btn)
         self.controls_layout.addStretch()
         self.convert_btn = QPushButton("Convert")
         self.convert_btn.setEnabled(False)
-        self.convert_btn.clicked.connect(self.convert_files)
+        self.convert_btn.clicked.connect(self.convert_file)
         self.controls_layout.addWidget(self.convert_btn)
-
-        self.file_list.model().rowsInserted.connect(
-            lambda _: self.convert_btn.setEnabled(self.file_list.count() > 0)
-        )
-
-        self.file_list.model().rowsRemoved.connect(
-            lambda _: self.convert_btn.setEnabled(self.file_list.count() > 0)
-        )
 
         self.out_types_widget = QWidget()
         self.out_types_layout = QHBoxLayout(self.out_types_widget)
@@ -89,26 +84,25 @@ class ConvertToWidget(QWidget):
         self.out_types_layout.addStretch()
         self.out_types_layout.addWidget(self.headers_check_box)
 
-        self._layout.addWidget(self.file_list)
-        self._layout.addStretch()
         self._layout.addWidget(self.out_types_widget)
         self._layout.addWidget(self.progress_bar)
         self._layout.addWidget(self.controls_widget)
 
-    def open_files(self) -> None:
-        self.paths, _ = QFileDialog.getOpenFileNames(
+    def on_pages_change(self, count: int) -> None:
+        self.convert_btn.setEnabled(count > 0)
+
+    def open_file(self) -> None:
+        self.path, _ = QFileDialog.getOpenFileName(
             self, "Open Files", QDir.homePath(), "PDFs (*.pdf)"
         )
 
-        if len(self.paths) == 0:
+        if len(self.path) < 1:
             return
 
-        self.file_list.addItems(self.paths)
+        self.list_widget.render_thumbnails(self.path)
 
-    def convert_files(self) -> None:
-        self.worker = Worker(
-            [self.file_list.item(i).text() for i in range(self.file_list.count())]
-        )
+    def convert_file(self) -> None:
+        self.worker = Worker(self.path)
         self.worker.started.connect(self.progress_bar.show)
         self.worker.total_ready.connect(self.progress_bar.setMaximum)
         self.worker.progress.connect(self.progress_bar.setValue)
